b998d2f1e9e2a300a50dd43677a79c068454e900
Abhi-1301 <90414121+Abhi-1301@users.noreply.github.com>
2022-01-20 14:36:43 +0530

    Create Bubble Sort.cpp

---
+++ Bubble Sort.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+#define mod 1000000007
+signed main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				int k = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = k;
+			}
+		}
+	}
+	int l = n / 3, r = (2 * n) / 3 - 1;
+	while (l < r)
+	{
+		int k = a[l];
+		a[l] = a[r];
+		a[r] = k;
+		l++;
+		r--;
+	}
+	for (auto x : a)
+		cout << x << " ";
+
+	return 0;
+}
